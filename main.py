8231ba1407f45acd2d18dbbe2c7fc3d1071ff28a
hiago <hiagouchoa173@gmail.com>
2026-07-29 13:19:21 -0300

    B: Movement working

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-BLACK = (0,0,0)
-while True:
-    log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    screen.fill("black")
-    pygame.display.flip()
 def main() -> None:
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0.0
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        screen.fill("black")
+        dt = clock.tick(60)/1000
+        for thing in drawable:
+            thing.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+        pygame.display.flip()
     
 main()
